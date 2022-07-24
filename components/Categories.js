5a81f6ebfe7e47dc3591d49db9fde546edc22ea8
Abdul Rafay Khan <rafaykhan.work@gmail.com>
2022-07-25 02:27:26 +0300

    first commit

---
+++ components/Categories.js
@@ -0,0 +1,29 @@
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+import CategoryCard from './CategoryCard'
+
+const Categories = () => {
+    return (
+        <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{
+                paddingHorizontal: 15,
+                paddingTop: 10,
+            }}
+        >
+            <CategoryCard imgUrl="https://links.papareact.com/wru" 
+            title="Testing 1" />
+            <CategoryCard imgUrl="https://links.papareact.com/wru" 
+            title="Testing 2" />
+            <CategoryCard imgUrl="https://links.papareact.com/wru" 
+            title="Testing 3" />
+            <CategoryCard imgUrl="https://links.papareact.com/wru" 
+            title="Testing 4" />
+            <CategoryCard imgUrl="https://links.papareact.com/wru" 
+            title="Testing 5" />
+        </ScrollView>
+    )
+}
+
+export default Categories
---
+++ screens/HomeScreen.js
@@ -0,0 +1,60 @@
+import { View, Text, Image, TextInput, ScrollView } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { UserIcon, ChevronDownIcon, SearchIcon,  AdjustmentsIcon } from "react-native-heroicons/outline";
+import Categories from '../components/Categories';
+
+
+const HomeScreen = () => {
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false
+        })
+    }, [])
+
+    return (
+        <SafeAreaView className="bg-white pt-5">
+            {/* Header */}
+            <View className="flex-row pb-3 item-center mx-4 space-x-2">
+                <Image
+                    source={{
+                        uri: "https://links.papareact.com/wru"
+                    }}
+                    className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+                />
+                <View className="flex-1">
+                    <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+                    <Text className="font-bold text-xl">Current Location
+                     <ChevronDownIcon size={20} color="#00CCBB"/>
+                    </Text>
+                </View>
+                <UserIcon size={35} color="#00CCBB"/>
+            </View>
+            
+            {/* Search */}
+            <View className="flex-row space-x-2 items-center pb-2 mx-4">
+                <View className="flex-row space-x-2 bg-gray-200 flex-1">
+                    <SearchIcon/>
+                    <TextInput placeholder='Restaurants and Cuisines'
+                     keyboardType='default'
+                    />
+                </View>
+                <AdjustmentsIcon color="#00CCBB"/>
+            </View>
+
+            {/* Body */}
+            <ScrollView>
+                {/* Categories */}
+                 <Categories/>
+
+                {/* Featured Rows*/}
+
+            </ScrollView>
+        </SafeAreaView>
+    )
+}
+
+export default HomeScreen
