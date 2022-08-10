1c0bcc0964854f199f986e668bd81f1f0533bfb1
Abdul Rafay Khan <rafaykhan.work@gmail.com>
2022-08-11 00:13:55 +0300

    res screen

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -40,7 +40,7 @@ const RestaurantCard = ({
                 }
                 className="h-36 w-64 rounded-sm"
             />
-            <View className="px-3 pb-4">
+            <View className="px-3 pb-4 flex-wrap">
                 <Text className="font-bold text-lg pt-2">{title}</Text>
                 <View className="flex-row items-center space-x-1">
                     <StarIcon color="green" opacity={0.5} size={22} />
@@ -48,9 +48,9 @@ const RestaurantCard = ({
                         <Text className="text-green-500">{rating}</Text> .
                         {genre}</Text> */}
                 </View>
-                <View className="flex-row items-center space-x-1 flex-shrink-0">
+                <View className="flex-row items-center space-x-1">
                     <LocationMarkerIcon color="gray" opacity={0.4} size={22} />
-                    <Text className="text-xs text-gray-500 " style={{
+                    <Text className="text-xs text-gray-500  " style={{
                         flexShrink: 2
                     }}>Nearby  -  {address}</Text>
                 </View>
--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,6 +1,7 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation, useRoute } from '@react-navigation/native'
+import { ArrowLeftIcon, ChevronRightIcon, LocationMarkerIcon, QuestionMarkCircleIcon, StarIcon } from 'react-native-heroicons/outline';
 
 const RestaurantScreen = () => {
 
@@ -26,16 +27,53 @@ const RestaurantScreen = () => {
         })
     }, [])
 
-    console.log('img', imgUrl)
+
     return (
         <ScrollView>
-            <View className="relativeaa">
-            <Image
-                source={
-                    imgUrl
-                }
-                className="w-full h-56 bg-gray-300 p-4"
-            />
+            <View className="relative">
+                <Image
+                    source={
+                        imgUrl
+                    }
+                    className="w-full h-56 bg-gray-300 p-4"
+                />
+                <TouchableOpacity
+                    onPress={() => navigation.goBack()}
+                    className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full">
+                    <ArrowLeftIcon size={20} color="#00CCBB" />
+                </TouchableOpacity>
+            </View>
+            <View className="bg-white">
+                <View className="pt-4 px-4">
+                    <Text className="text-2xl font-bold">{title}</Text>
+                    <View className="flex-row space-x-2 my-1 flex-wrap">
+                        <View className="flex-row items-center space-x-1">
+                            <StarIcon color="green" opacity={0.5} size={22} />
+                            <Text className="text-green-500">4.8</Text>
+                        </View>
+
+                        <View className="flex-row items-center space-x-1 flex-wrap">
+                            <LocationMarkerIcon color="gray" opacity={0.4} size={22} />
+                            <Text className="text-xs text-gray-500 " style={{
+                                flexShrink: 2
+                            }}>Nearby  -  {address}</Text>
+                        </View>
+                    </View>
+                    <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
+                </View>
+                <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y
+                 border-gray-300
+                ">
+                    <QuestionMarkCircleIcon color="gray" opacity={0.6} size={22}/>
+                    <Text className="pl-2 flex-1 text-md font-bold">Have a food allergy?</Text>
+                    <ChevronRightIcon color="#00CCBB"/>
+                </TouchableOpacity>
+            </View>
+
+            <View>
+                <Text className="px-4 pt-6 mb-4 font-bold text-xl">
+                    Menu
+                </Text>
             </View>
         </ScrollView>
     )
